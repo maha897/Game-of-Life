f6919513968eaa96edf39bb7ecfcc22f1de05d19
mahari <mahari@uio.no>
2023-11-19 14:56:34 +0000

    added code skeleton and started celle.java

---
+++ Celle.java
@@ -0,0 +1,14 @@
+public class Celle{
+    boolean levende; 
+    Celle[] naboer;
+    int antNaboer;
+    int antLevendeNaboer;
+
+    public Celle(){
+        levende = false;
+        antNaboer = 0;
+        antLevendeNaboer = 0;
+    }
+    
+    
+}
